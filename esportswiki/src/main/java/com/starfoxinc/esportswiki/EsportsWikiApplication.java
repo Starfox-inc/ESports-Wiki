797878ca6b31f7c0a6d7c981a4f5acfe3742541c
ByJH <alkas001@csusm.edu>
2024-02-21 17:13:32 -0800

    Add Spring backend

---
+++ esportswiki/src/main/java/com/starfoxinc/esportswiki/EsportsWikiApplication.java
@@ -0,0 +1,13 @@
+package com.starfoxinc.esportswiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsWikiApplication.class, args);
+	}
+
+}
---
+++ esportswiki/src/test/java/com/starfoxinc/esportswiki/EsportsWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starfoxinc.esportswiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
